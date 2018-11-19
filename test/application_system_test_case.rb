429a7d5c438a9d32367e0a143efe6e8fe9ad3e4d
Khalid EL MAJDOUBY <k.elmajdouby@gmail.com>
2018-11-19 13:57:01 +0000

    done

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
-require "test_helper"
+require "application_system_test_case"
 
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
 end
